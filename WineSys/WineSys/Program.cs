ed042e93ed7a1021f446c0a67fecb3f7e049907a
Casper Bech <casperbech@pm.me>
2024-01-03 14:34:58 +0100

    Initial

---
+++ WineSys/WineSys/Program.cs
@@ -0,0 +1,20 @@
+﻿using DVI_Access_Lib;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        DVI_Stock stock = new DVI_Stock("http://docker.data.techcollege.dk:5051");
+
+        Console.WriteLine("Vine på lager:");
+
+        List<Wine> winesInStock = stock.WinesOnStock();
+
+        foreach (Wine wine in winesInStock)
+        {
+            Console.WriteLine(wine.WineName + " " + wine.NumInStock);
+        }
+
+        Console.ReadLine();
+    }
+}
