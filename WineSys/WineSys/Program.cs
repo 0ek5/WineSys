5a2a53ada3c7f2a30ed1f91401b865cc5c9f5f07
0ek5 <0ek5@protonmail.com>
2024-01-04 14:47:33 +0100

    Added boxes, hum and temp

--- WineSys/WineSys/Program.cs
+++ WineSys/WineSys/Program.cs
@@ -1,20 +1,30 @@
-﻿using DVI_Access_Lib;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVI_Access_Lib;
 using Konsole;
 
-internal class Program
+namespace DVI_Demo
 {
-
-    static void Main(string[] args)
+    internal class Program
     {
-        var winListWindow = Window.OpenBox("Vine på lager", 60, 25);
-
-        DVI_Stock stock = new DVI_Stock("http://docker.data.techcollege.dk:5051");
-
-        List<Wine> winesInStock = stock.WinesOnStock();
-
-        foreach (Wine wine in winesInStock)
+        static void Main(string[] args)
         {
-            winListWindow.WriteLine(wine.WineName + " " + wine.NumInStock);
+            var winListWindow = Window.OpenBox("Vine på lager", 30, 30, 20, 20);
+            var winCurrTempCurrHum = Window.OpenBox("CurrTemp", 50, 50, 10, 10);
+
+            DVI_Climate climate = new DVI_Climate("http://docker.data.techcollege.dk:5051");
+            DVI_Stock stock = new DVI_Stock("http://docker.data.techcollege.dk:5051");
+  
+
+            winListWindow.WriteLine("Vine på lager:");
+
+            foreach (Wine wine in stock.WinesOnStock())
+                winListWindow.WriteLine(wine.WineName + " " + wine.NumInStock);
+
+            winCurrTempCurrHum.WriteLine(climate.CurrTemp().ToString() + "\n" + climate.CurrHum().ToString());
         }
     }
 }
