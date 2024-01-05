aa1e6812140c9eea8cf899c48b44952cb64853b1
0ek5 <0ek5@protonmail.com>
2024-01-05 13:16:55 +0100

    Added extended logic and console frontend

--- WineSys/WineSys/Program.cs
+++ WineSys/WineSys/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using DVI_Access_Lib;
 using Konsole;
@@ -15,23 +17,58 @@ namespace DVI_Demo
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(150,50);
-
-            // int testValLow = 0;
-            // int testValHigh = 1000;
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Black;
 
-            var stockX = Window.OpenBox("stockX", 30, 50, 75, 50, new BoxStyle());
+            // Remember to add time and date instead of only timezone!!!
 
-            var winListWindow = Window.OpenBox("Vine på lager", 5, 2, 50, 30, new BoxStyle()
+            TimeZoneInfo dktime = TimeZoneInfo.FindSystemTimeZoneById("Romance Standard Time");
+            TimeZoneInfo calitime = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            TimeZoneInfo austime = TimeZoneInfo.FindSystemTimeZoneById("W. Australia Standard Time");
+
+
+
+
+            // Window.OpenBox(string title, int sx, int sy, int width, int height, BoxStyle style)            
+
+            var winListWindow = Window.OpenBox("Lagerbeholdning for høj", 5, 2, 50,16, new BoxStyle()
             {
                 ThickNess = LineThickNess.Double,
                 Title = new Colors(ConsoleColor.White, ConsoleColor.Red)
             });
 
-            var winCurrTempCurrHum = Window.OpenBox("Temperatur og Luftfugtighed",5,32,50,5, new BoxStyle()
+            //Resize - Height; placement
+            
+            var winListWindow2 = Window.OpenBox("Lagerbeholdning lav", 5, 17, 50, 15, new BoxStyle()
+            {
+                ThickNess = LineThickNess.Double,
+                Title = new Colors(ConsoleColor.White, ConsoleColor.Red)
+            });
+
+            var Vin = Window.OpenBox("Vin", 55, 2, 100, 30, new BoxStyle()
+            {
+                ThickNess = LineThickNess.Double,
+                Title = new Colors(ConsoleColor.White, ConsoleColor.Red)
+
+            });
+
+            // Split this box
+
+            var winCurrTempCurrHum = Window.OpenBox("Temperatur og Luftfugtighed",5,32,50,6, new BoxStyle()
+            {
+                ThickNess = LineThickNess.Double,
+                Title = new Colors(ConsoleColor.White, ConsoleColor.Red)
+        });
+
+            var Box3 = Window.OpenBox("Box3", 55, 32, 50, 6, new BoxStyle()
+            {
+                ThickNess = LineThickNess.Double,
+                Title = new Colors(ConsoleColor.White, ConsoleColor.Red)
+            });
+
+            var Box4 = Window.OpenBox("Box4", 105, 32, 50, 6, new BoxStyle()
             {
                 ThickNess = LineThickNess.Double,
                 Title = new Colors(ConsoleColor.White, ConsoleColor.Red)
@@ -41,20 +78,28 @@ namespace DVI_Demo
             DVI_Climate climate = new DVI_Climate("http://docker.data.techcollege.dk:5051");
             DVI_Stock stock = new DVI_Stock("http://docker.data.techcollege.dk:5051");
 
-            foreach (Wine wine in stock.WinesOnStock())
-                stockX.WriteLine(wine.WineName + " " + wine.BaseGrape);
-
             while (true)
             {
 
-            winListWindow.Clear();
+                Vin.Clear();
+                foreach (Wine wine in stock.StockOverThreshold())
+                    Vin.WriteLine(wine.WineName + " " + "[" + wine.NumInStock + "]");
 
-                foreach (Wine wine in stock.WinesOnStock())
-                    stockX.WriteLine(wine.WineName + "\n" + wine.BaseGrape + "\n");
+                winListWindow.Clear();
+                foreach (Wine wine in stock.StockOverThreshold())
+                winListWindow.WriteLine(wine.WineName + " " + "[" +wine.NumInStock + "]");
 
-                foreach (Wine wine in stock.WinesOnStock())
-                winListWindow.WriteLine(wine.WineName + " " + wine.NumInStock);
-            
+                
+                winListWindow2.Clear();
+                foreach (Wine wine in stock.StockUnderThreshold())
+                    winListWindow2.WriteLine(wine.WineName + "[" + wine.NumInStock + "]");
+
+
+                Box3.Clear();
+                Box3.WriteLine(dktime + "\n" + calitime + "\n" + austime);
+
+                //Split
+                
                 winCurrTempCurrHum.Clear(); 
                 winCurrTempCurrHum.WriteLine(climate.CurrTemp().ToString() + "°" + "                                " + climate.CurrHum().ToString() + "%");
 
@@ -64,8 +109,6 @@ namespace DVI_Demo
                     Console.Beep();
                 }
                 
-                    
-
                 if (climate.CurrTemp() < climate.MinTemp())
                 {   
                     winCurrTempCurrHum.WriteLine("LOW TEMP!");
@@ -90,6 +133,15 @@ namespace DVI_Demo
                     Console.Beep();
                 }
 
+                if (climate.CurrTemp() < climate.MaxTemp() && climate.CurrTemp() > climate.MinTemp())
+                {
+                    winCurrTempCurrHum.WriteLine("WORKING");
+                }
+
+                if (climate.CurrHum() < climate.MaxHum() && climate.CurrHum() > climate.MinHum())
+                {
+                    winCurrTempCurrHum.WriteLine("                                     WORKING");
+                }
 
                 Thread.Sleep(10000);
 
